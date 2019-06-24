b120e31288d5afd6ce80ef44e47833975bb1176a
root <g.r.almeida@live.com>
2019-06-24 00:06:46 -0300

    Making the dispatch of the action addTodo

--- src/Todo.js
+++ src/Todo.js
@@ -1,65 +1,26 @@
-import React, { Component } from 'react'
-import { View, StyleSheet, Button } from 'react-native'
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import rootReducer from './redux/reducers'
-import Input from './components/Input'
+import Form from './components/Form'
 
 const store = createStore(rootReducer)
 
-export default class Todo extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      text: ''
-    }
-  }
-
-  onChangeText (text) {
-    this.setState({
-      text
-    })
-  }
-
-  onPress () {
-    console.log(this.state.text)
-  }
-
-  render () {
-    const { text } = this.state
-
-    return (
-      <Provider store={store}>
-        <View style={styles.formContainer}>
-          <View style={styles.inputContainer}>
-            <Input
-              onChangeText={text => this.onChangeText(text)}
-              value={text}
-            />
-          </View>
-          <View style={styles.buttonContainer}>
-            <Button
-              onPress={() => this.onPress()}
-              title='add'
-            />
-          </View>
-        </View>
-      </Provider>
-    )
-  }
-}
+const Todo = () => (
+  <Provider store={store}>
+    <View style={styles.formContainer}>
+      <Form />
+    </View>
+  </Provider>
+)
 
 const styles = StyleSheet.create({
   formContainer: {
     paddingTop: 30,
     flexDirection: 'row'
-  },
-  inputContainer: {
-    flex: 4
-  },
-  buttonContainer: {
-    flex: 1
   }
 })
+
+export default Todo
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,38 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import React, { Fragment } from 'react'
+import { View, Button, StyleSheet } from 'react-native'
+import { connect } from 'react-redux'
 
-export default class Form extends Component {
-  render () {
-    return (
-      <View>
-        <Text>Form</Text>
-      </View>
-    )
+import { addTodo } from '../redux/action-creators'
+import Input from './Input'
+
+const Form = ({ addTodo }) => (
+  <Fragment>
+    <View style={styles.inputContainer}>
+      <Input
+        onChangeText={() => null}
+        value=''
+      />
+    </View>
+    <View style={styles.buttonContainer}>
+      <Button
+        onPress={addTodo}
+        title='add'
+      />
+    </View>
+  </Fragment>
+)
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 4
+  },
+  buttonContainer: {
+    flex: 1
   }
+})
+
+const mapDispatchToProps = {
+  addTodo
 }
+
+export default connect(null, mapDispatchToProps)(Form)
